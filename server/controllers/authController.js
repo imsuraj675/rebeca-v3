3cac4a3a0553cdd909cc492a8c53668cfd3a566f
KakarotDB <mrigajpersonal@gmail.com>
2026-03-07 09:59:49 +0530

    Updated server/controllers/authController.js to include a verifying admin passkey for the admin dashboard

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -93,7 +93,7 @@ exports.logout = (req, res) => {
     res.status(200).json({ status: "success" });
 };
 
-//verifying admin passkey
+//verifying admin passkey 
 exports.verifyPasskey = catchAsync(async (req, res, next) => {
     const { passkey } = req.body;
 
